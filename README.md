2a045fa7e836660f09da14f1bff56e0a90605254
Joao Cavalheiro <jcavalheiro@suse.de>
2019-02-20 16:53:48 +0000

    Initial commit (v0.1)

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# prometheus-libvirt_sd
-Prometheus libvirt service discovery
+# Prometheus libvirt Service Discovery
+
+Prometheus offers a variety of service discovery options for discovering scrape targets, but those do not include libvirt. This tool connects to libvirt hypervisors and generates Prometheus scrape target configurations, taking advantage of the [file-based service discovery](https://prometheus.io/docs/prometheus/latest/configuration/configuration/#%3Cfile_sd_config) mechanism provided by Prometheus.
+
+You can define a set of rules on how to group and label libvirt guests. Those rules are defined in a [configuration file](prometheus-libvirt_sd.yml) and support _regular expressions_ using the same general syntax used by Perl, Python, and other languages. Complete rules are described at https://golang.org/s/re2syntax.
+
+**Currently, the guest FQDN's are resolved via a "guessing" method** that combines each guest's VM name with the hypervisor's domain name. _This only works under strict conditions where the DNS naming of guests follows this convention_. There is work in progress development to support real name resolution using libvirt guest agent.
+
+Example rule:
+```yaml
+- labels: { job: libvirt }
+  domains:
+    - match: .*
+      ports: [9100]
+```
+
+Example output:
+```yaml
+- targets:
+  - guest-1.local:9100
+  - guest-2.local:9100
+  - guest-3.local:9100
+  labels:
+    hypervisor: hypervisor.local
+    job: libvirt
+```
+
+## Prometheus Configuration
+
+One result file will be generated per each hypervisor defined in the configuration settings, named after the hypervisor FQDN. Please remember to adjust your `prometheus.yml` configuration file to use the file service discovery mechanism and point it to the output location of this tool.
+
+Example configuration section of prometheus.yml:
+```yaml
+- job_name: 'overwritten-default'
+  file_sd_configs:
+   - files: ['/data/prometheus/scrape-config/*.yml']
+```
+
+## License
+
+This project has been released under the MIT license. Please see the LICENSE.md file for more details.
---
+++ prometheus-libvirt_sd.go
@@ -0,0 +1,204 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"fmt"
+	"os"
+	"regexp"
+	"sync"
+	"time"
+
+	libvirt "github.com/libvirt/libvirt-go"
+	// "github.com/libvirt/libvirt-go-xml"
+	"gopkg.in/yaml.v2"
+)
+
+// Constants
+
+const DEFAULT_CONFIG_FILE = "prometheus-libvirt_sd.yml"
+
+// Structures
+
+type Group struct {
+	Labels map[string]string
+	Domains []Domain
+}
+
+type Domain struct {
+	Match string
+	Ports []string
+	Labels map[string]string
+}
+
+type Config struct {
+	OutputDir string
+	PollingInterval int
+	Hosts []string
+	Groups []Group
+}
+
+// Result structure
+
+type PromScrapeGroup struct {
+	Targets []string
+	Labels  map[string]string
+}
+
+var config Config
+
+// Given a list of domains obtained from an hypervisor,
+// find the ones that match configured groups
+func findMatchingDomains(domainList []libvirt.Domain, hvDomain string,
+	domainExpr string) ([]string) {
+	result := []string{}
+	for _, domainObj := range domainList {
+		domainName, _ := domainObj.GetName()
+
+		// xmldoc, _ := domainObj.GetXMLDesc(0)
+		// domcfg := &libvirtxml.Domain{}
+		// domcfg.Unmarshal(xmldoc)
+		// fmt.Printf("mac %+v\n", domcfg.Devices.Interfaces[0].MAC.Address)
+		// fmt.Printf("net %+v\n", domcfg.Devices.Interfaces[0].Source.Network.Network)
+		// fmt.Printf("x: %v\n", domainName)
+
+		fqdn := domainName + hvDomain
+		match, _ := regexp.MatchString(domainExpr, fqdn)
+		if match {
+			result = append(result, domainName)
+		}
+	}
+	return result
+}
+
+// Generate scrape configuration for a given Libvirt hypervisor
+func queryLibvirtHypervisor(uri string) (int, error) {
+	conn, err := libvirt.NewConnect(uri)
+	if err != nil {
+		return -1, err
+	}
+	defer conn.Close()
+
+	// Get all domains from libvirt hypervisor
+	hvDomain := getHypervisorDomainName(conn)
+	promConfig := []PromScrapeGroup{}
+	domainList, _ := conn.ListAllDomains(0)
+
+	// Test
+	// networkDetails, _ := conn.LookupNetworkByName("default")
+	// net, _ := networkDetails.GetDHCPLeases()
+	// domcfg := &libvirtxml.Domain{}
+	// domcfg.Unmarshal(xmldoc)
+	// fmt.Printf("network: %+v", net)
+
+
+	for _, groupConfig := range config.Groups {
+		for _, domainConfig := range groupConfig.Domains {
+			// Declare base structure for prom scrape group
+			promScrapeGroup := PromScrapeGroup{
+				Targets: []string{},
+				Labels: map[string]string{
+					"hypervisor" : hvDomain[0]}}
+			// Check if the domain config iteratee exists on hypervisor
+			matchedDomains := findMatchingDomains(domainList, hvDomain[1], domainConfig.Match)
+			if len(matchedDomains) > 0 {
+				// Add group labels
+				for k, v := range groupConfig.Labels {
+				    promScrapeGroup.Labels[k] = v
+				}
+				// Add domain labels
+				for k, v := range domainConfig.Labels {
+				    promScrapeGroup.Labels[k] = v
+				}
+				// Add scrape targets and ports
+				for _, dom :=  range matchedDomains {
+					for _, port := range domainConfig.Ports {
+						promScrapeGroup.Targets = append(promScrapeGroup.Targets, dom + "." + hvDomain[1] + ":" + port)
+					}
+				}
+				promConfig = append(promConfig, promScrapeGroup)
+			}
+		}
+	}
+	return writePromConfig(hvDomain[0], promConfig)
+}
+
+
+// Given a libvirt connection, return the hypervisor domain name
+func getHypervisorDomainName(conn *libvirt.Connect) []string {
+	hostName, _ := conn.GetHostname()
+	re, _ := regexp.Compile(`\w+\.(.*)`)
+	match := re.FindStringSubmatch(hostName)
+	domain := "local"
+	if len(match) > 1 {
+		domain = match[1]
+	}
+	return []string{hostName, domain}
+}
+
+// Generate the yaml files for Prometheus based on a scrape config structure
+func writePromConfig(hvDomain string, promConfig []PromScrapeGroup) (int, error) {
+	ymlPromConfig, _ := yaml.Marshal(promConfig)
+	err := ioutil.WriteFile(config.OutputDir+"/"+hvDomain+".yml", []byte(ymlPromConfig), 0644)
+	fmt.Printf("Scrape config for %v: %v", hvDomain, string(ymlPromConfig))
+	if err != nil {
+		return -1, err
+	}
+	return len(promConfig), nil
+}
+
+// Error handler
+func fatalErrorHandler(e error, msg string) {
+	if e != nil {
+		fmt.Printf("ERROR: %s\n", e.Error())
+		fmt.Printf("ERROR: %s\n", msg)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	// Parse command line arguments
+	var (
+		configFile = flag.String("config", DEFAULT_CONFIG_FILE, "Path to config file")
+	)
+	flag.Parse()
+
+	// Set defaults
+	config = Config{PollingInterval: 120, OutputDir: "/tmp"}
+
+	// Load configuration file
+	dat, err := ioutil.ReadFile(*configFile)
+	fatalErrorHandler(err, "Unable to read configuration file - please specify the correct location using --config=file.yml")
+	err = yaml.Unmarshal([]byte(dat), &config)
+	fatalErrorHandler(err, "Unable to parse configuration file")
+	// Trace.Printf("\n%v\n", config)
+
+	// Output some info about supplied config
+	fmt.Printf("Using config file: %v\n", *configFile)
+	fmt.Printf("\tpolling interval: %d seconds\n", config.PollingInterval)
+	fmt.Printf("\toutput dir: %v\n", config.OutputDir)
+
+	// Loop through list of configured hypervisors
+	for {
+		var wg sync.WaitGroup
+		wg.Add(len(config.Hosts))
+		for _, host := range config.Hosts {
+			go func(host string) {
+				defer wg.Done()
+				fmt.Printf("Querying hypervisor %+v...\n", host)
+				c, err := queryLibvirtHypervisor(host)
+				if err != nil {
+					fmt.Printf("ERROR: %v - %v+\n", host, err)
+				} else {
+					fmt.Printf("%v: scrape config updated (%d groups).\n", host, c)
+				}
+			}(host)
+		}
+		wg.Wait()
+		if config.PollingInterval > 0 {
+			time.Sleep(time.Duration(config.PollingInterval) * time.Second)
+		} else {
+			break
+		}
+	}
+}
